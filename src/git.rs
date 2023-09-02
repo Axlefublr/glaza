eaf91c55635f90e5a8738fe14a80b97e430df26b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-09-03 04:01:48 +0800

    switch to 4 spaces because fighting with rust is exhausting

--- src/git.rs
+++ src/git.rs
@@ -2,16 +2,16 @@ use std::path::Path;
 use std::process::Command;
 
 pub fn add_commit(working_dir: &Path, file: &Path, message: String) {
-	let command = format!(
-		"git add {} && git commit -m \"{}\"",
-		file.display(),
-		message
-	);
+    let command = format!(
+        "git add {} && git commit -m \"{}\"",
+        file.display(),
+        message
+    );
 
-	Command::new("sh")
-		.arg("-c")
-		.arg(&command)
-		.current_dir(working_dir)
-		.output()
-		.unwrap();
+    Command::new("sh")
+        .arg("-c")
+        .arg(&command)
+        .current_dir(working_dir)
+        .output()
+        .unwrap();
 }
--- src/show.rs
+++ src/show.rs
@@ -2,56 +2,56 @@ use clap::Subcommand;
 
 #[derive(Subcommand)]
 pub enum ShowCommands {
-	/// Open a show you're currently watching in your $BROWSER.
-	/// Works by concatenating the link you specified for the show and the episode you last set with `floral_barrel show set episode` + 1, effectively resulting in "watch the next episode".
-	Watch {
-		#[arg(short, long)]
-		show: String,
-	},
-	/// Open a show you're currently watching in your $BROWSER, for downloading.
-	/// Works by concatenating the link you specified for the show and the episode you last set with `floral_barrel show set download` + 1, effectively resulting in "download the next episode"
-	Download {
-		#[arg(short, long)]
-		show: String,
-	},
-	/// Finish a show, putting it in your watched list with the date of finishing.
-	Finish {},
-	/// Drop a show, putting it in your watched list tagged as dropped, with the date of dropping.
-	Drop {},
-	/// Start a new show by specifying a link to it.
-	/// For `show watch` and `show download` to work properly, strip the end of the link so when you append a number to it, it results in the correct link to the episode.
-	New {},
-	/// List all the shows you're currently watching in the format of `showName - ep99 - dn99`
-	List {},
-	/// Print the entirety of the watched file, effectively showing all shows you've ever watched.
-	Past {},
-	/// Update how many episodes of a show you're currently watching you've watched / downloaded.
-	Set {
-		#[command(subcommand)]
-		action: SetActions,
-	},
+    /// Open a show you're currently watching in your $BROWSER.
+    /// Works by concatenating the link you specified for the show and the episode you last set with `floral_barrel show set episode` + 1, effectively resulting in "watch the next episode".
+    Watch {
+        #[arg(short, long)]
+        show: String,
+    },
+    /// Open a show you're currently watching in your $BROWSER, for downloading.
+    /// Works by concatenating the link you specified for the show and the episode you last set with `floral_barrel show set download` + 1, effectively resulting in "download the next episode"
+    Download {
+        #[arg(short, long)]
+        show: String,
+    },
+    /// Finish a show, putting it in your watched list with the date of finishing.
+    Finish {},
+    /// Drop a show, putting it in your watched list tagged as dropped, with the date of dropping.
+    Drop {},
+    /// Start a new show by specifying a link to it.
+    /// For `show watch` and `show download` to work properly, strip the end of the link so when you append a number to it, it results in the correct link to the episode.
+    New {},
+    /// List all the shows you're currently watching in the format of `showName - ep99 - dn99`
+    List {},
+    /// Print the entirety of the watched file, effectively showing all shows you've ever watched.
+    Past {},
+    /// Update how many episodes of a show you're currently watching you've watched / downloaded.
+    Set {
+        #[command(subcommand)]
+        action: SetActions,
+    },
 }
 
 #[derive(Subcommand)]
 pub enum SetActions {
-	/// Set the episode you just watched of a show
-	Episode {
-		#[arg(short, long)]
-		show: String,
-		#[arg(short, long)]
-		episode: u32,
-	},
-	/// Set the episode you just downloaded of a show
-	Download {
-		#[arg(short, long)]
-		show: String,
-		#[arg(short, long)]
-		episode: u32,
-	},
-	Link {
-		#[arg(short, long)]
-		show: String,
-		#[arg(short, long)]
-		link: String,
-	},
+    /// Set the episode you just watched of a show
+    Episode {
+        #[arg(short, long)]
+        show: String,
+        #[arg(short, long)]
+        episode: u32,
+    },
+    /// Set the episode you just downloaded of a show
+    Download {
+        #[arg(short, long)]
+        show: String,
+        #[arg(short, long)]
+        episode: u32,
+    },
+    Link {
+        #[arg(short, long)]
+        show: String,
+        #[arg(short, long)]
+        link: String,
+    },
 }
