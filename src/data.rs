4fbb6ee3f4b402305ea4e33dfe77f97626d86e3c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-09-03 00:49:51 +0800

    impl data initializer

---
+++ src/data.rs
@@ -0,0 +1,43 @@
+use dirs;
+use std::fs;
+use std::fs::File;
+use std::path::Path;
+use std::path::PathBuf;
+
+const DATA_DIR: &str = "floral_barrel";
+const SHOWS_FILE: &str = "shows.json";
+const WATCHED_FILE: &str = "watched.txt";
+const WATCH_LATER_FILE: &str = "watch-later.txt";
+
+fn get_floral_barrel_dir() -> PathBuf {
+	let data_dir = dirs::data_dir().unwrap();
+	data_dir.join(DATA_DIR)
+}
+
+fn create(parent_buf: &Path, file_path: &str) -> PathBuf {
+	let file = parent_buf.join(file_path);
+	File::create(&file).unwrap();
+	file
+}
+
+pub struct DataFiles {
+	pub shows: PathBuf,
+	pub watched: PathBuf,
+	pub watch_later: PathBuf,
+	pub floral_barrel: PathBuf,
+}
+
+impl DataFiles {
+	pub fn create() -> Self {
+		let floral_barrel = get_floral_barrel_dir();
+		let shows = create(&floral_barrel, SHOWS_FILE);
+		let watched = create(&floral_barrel, WATCHED_FILE);
+		let watch_later = create(&floral_barrel, WATCH_LATER_FILE);
+		Self {
+			shows,
+			watched,
+			watch_later,
+			floral_barrel,
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use args::UserCommands;
+use clap::Parser;
+use show::SetActions;
+use show::ShowCommands;
+use crate::args::Args as Args;
+
 mod args;
 mod show;
-mod set;
-mod list;
+mod wl;
+mod data;
 
 fn main() {
 	println!("Hello, world!");
